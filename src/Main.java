f7493cd3f9a01dea30e481eb9875fe787348576c
Sheldon <tinarychina@gmail.com>
2019-07-19 01:33:55 +0800

    添加Person类

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import com.sheldon.hello.Person;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person p1 = new Person();
+        p1.setName("Sheldon");
+        p1.setAge(18);
+
+        System.out.println("姓名：" + p1.getName());
+        System.out.println("年龄：" + p1.getAge());
     }
 }
